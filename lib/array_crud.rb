5f409f57895fadac9e4cf49280a9bbaee34b78ff
John Schmidley <tlg8n@outlook.com>
2019-09-15 16:49:00 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -6,8 +6,7 @@ def create_an_array
   ["dog", "cat", "bird", "fish"]
 end
 
-def add_element_to_end_of_array(array,element)
-  array << "element"
+def add_elemelement
 end
 
 def add_element_to_start_of_array(array, element)
