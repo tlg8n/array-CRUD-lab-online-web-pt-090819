94f15af681efa39c4440b35d2888503383c60fd3
John Schmidley <tlg8n@outlook.com>
2019-09-15 16:18:00 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,13 +1,13 @@
 def create_an_empty_array
-  
+  []
 end
 
 def create_an_array
-  
+  ["dog", "cat", "bird", "fish"]
 end
 
 def add_element_to_end_of_array(array, element)
-  
+  pets << "Snake"
 end
 
 def add_element_to_start_of_array(array, element)
